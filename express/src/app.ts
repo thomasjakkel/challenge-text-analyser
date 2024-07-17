513e3df07bbd0e102d85c929a00227badd88c366
thomasjakkel <thomas.jakkel@gmail.com>
2024-07-17 12:30:00 +0200

    backend: Added cors middleware

--- express/src/app.ts
+++ express/src/app.ts
@@ -2,9 +2,11 @@ import express, { Request, Response, Application, json } from "express"
 import { errorHandler } from "./middleware/errorHandler"
 import countersRouter from "./routes/counters"
 import taskItemsRouter from "./routes/task-items"
+import cors from "cors"
 
 const app: Application = express()
 
+app.use(cors())
 app.use(json())
 app.use((req, _res, next) => {
   console.log(`Received ${req.method} ${req.url}`)
